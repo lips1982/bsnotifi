81bb2117a5896132fc5766b836357a656f7ec399
lips1982 <42499047+lips1982@users.noreply.github.com>
2022-08-22 19:17:21 -0500

    Update inicio.sh

--- inicio.sh
+++ inicio.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+clear
+echo "INICIANDO"
+sudo service docker stop
+
+while getopts k: option
+do 
+    case "${option}"
+        in
+        k)SERVERNAME=${OPTARG};;
+
+    esac
+done
+echo "KEY_ACCES : $SERVERNAME"
+
+sed -i "s|SERVERNAME|${SERVERNAME}|g" daemon.json
+
+sudo cp -f daemon.json /etc/docker/
+
+sudo service docker start
 
 PUBLIC_IP=`wget http://ipecho.net/plain -O - -q ; echo`
 echo "$PUBLIC_IP"
@@ -10,4 +29,4 @@ docker build -t display .
 
 
 screen -S docker -d -m bash -c "docker run -it --rm -v $PWD/img:/app/Almacenamiento/img display"
-screen -S web -d -m bash -c "cd img/ && python3 -m http.server 8080"
+#screen -S web -d -m bash -c "cd img/ && python3 -m http.server 8080"
